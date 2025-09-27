e203b952ee7b63856093fbd9a70b175868968336
JuddBE <63137249+JuddBE@users.noreply.github.com>
2025-09-26 20:48:32 -0400

    Update main.py
    test database connection

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from typing import Optional
-
 from fastapi import FastAPI
+from pymongo import MongoClient
 
 app = FastAPI()
 
+# Connect to mongodb cluster
+MONGO_URI = "mongodb+srv://Judd:<db_password>@cluster0.s4nnpra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(MONGO_URI)
+db = client["main"]
+users = db["users"]
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"users": list(users)}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
